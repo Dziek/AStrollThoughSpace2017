70763f693eea1dbd61a229930d9290db24967e05
Dziek <hackingnasatips@gmail.com>
2017-11-30 13:49:43 +0000

    InitCommit

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float scaleUpRate = 1.1f;
+	public float scaleDownRate = 0.9f;
+	
+	public GameObject otherSpriteMask;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0) || Input.GetKey("space"))
+		{
+			transform.localScale = new Vector2(transform.localScale.x * scaleUpRate, transform.localScale.y * scaleUpRate);
+		}else{
+			transform.localScale = new Vector2(transform.localScale.x * scaleDownRate, transform.localScale.y * scaleDownRate);
+		}
+		
+		if (transform.localScale.x <= 0.05f)
+		{
+			Dead();
+		}
+	}
+	
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.gameObject.tag == "Border")
+		{
+			Dead();
+		}
+		
+		if (other.gameObject.tag == "Wall")
+		{
+			Dead();
+		}
+		
+		if (other.gameObject.tag == "WallMiddle")
+		{
+			if (transform.localScale.y < other.gameObject.transform.localScale.y)
+			{
+				Dead();
+			}
+		}
+	}
+	
+	void Dead () {
+		Debug.Log("OVER");
+		otherSpriteMask.SetActive(false);
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+	
+	public float speedMin = 1;
+	public float speedMax = 10;
+	
+	public float widthMin = 0.1f;
+	public float widthMax = 5f;
+	
+	private float currentSpeed;
+	
+	void OnEnable () {
+		currentSpeed = Random.Range(speedMin, speedMax);
+		
+		transform.localScale = new Vector2(Random.Range(widthMin, widthMax), transform.localScale.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(transform.right * -currentSpeed);
+	}
+}
