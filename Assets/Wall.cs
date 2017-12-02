6e00b36bb0640a6ba8c8b70070bc0b93dab233d5
Dziek <hackingnasatips@gmail.com>
2017-12-02 22:04:35 +0000

    Added Wall size changing
    Values need to be tweaked but feel pretty good about it

--- Assets/Wall.cs
+++ Assets/Wall.cs
@@ -7,7 +7,14 @@ public class Wall : MonoBehaviour {
 	public Range speed;
 	public Range width;
 	
-	public float scaleFactor = 4;
+	// 3.5f, 6f are good ones
+	public Range wallYValues = new Range(3.5f, 6f);
+	
+	// what the scale of the middle bit will be when the wall is closest together
+	public float minimumScale = 0.2f;
+	
+	// the larger the number, the smaller the gap between wall and middle bit will be
+	public float scaleFactor = 0.8f;
 	
 	public GameObject wallTopGO;
 	public GameObject wallMiddleGO;
@@ -19,6 +26,23 @@ public class Wall : MonoBehaviour {
 		currentSpeed = Random.Range(speed.min, speed.max);
 		
 		transform.localScale = new Vector2(Random.Range(width.min, width.max), transform.localScale.y);
+		
+		float newY = Random.Range(wallYValues.min, wallYValues.max);
+		// float newY = Random.Range(4, 6);
+		
+		wallTopGO.transform.position = new Vector2(wallTopGO.transform.position.x, newY);
+		wallBottomGO.transform.position = new Vector2(wallBottomGO.transform.position.x, -newY);
+		
+		float scaleValue = ((newY - wallYValues.min) * scaleFactor)+ minimumScale;
+		// float scaleValue = ((newY - 4) * 0.8f)+ 1;
+		
+		wallMiddleGO.transform.localScale = new Vector2(wallMiddleGO.transform.localScale.x, scaleValue);
+		
+		// float translateValue = Random.Range(0, 1.5f);
+		// float translateValue = 1;
+		
+		// wallTopGO.transform.Translate(Vector2.up * translateValue);
+		// wallBottomGO.transform.Translate(Vector2.up * -translateValue);
 		
 		// float scaleValue = Random.Range(0.5f, 1.5f);
 		// wallTopGO.transform.localScale = new Vector2(wallTopGO.transform.localScale.x, wallTopGO.transform.localScale.y * scaleValue);
